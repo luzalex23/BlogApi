612424e07ebe033323cbc5af33ea0c8408b478f5
luzalex23 <alexluzendreyravi@gmail.com>
2023-11-02 14:17:06 -0300

    Arquitetura da camada de infraestrutura

---
+++ Domain/Repositories/IPosts.cs
@@ -0,0 +1,15 @@
+﻿using Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public interface IPosts
+    {
+        List<PostDTO> GetPagination(int pageNumber, int pageQuantity);
+
+    }
+}
